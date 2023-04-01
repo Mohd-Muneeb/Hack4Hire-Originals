2b4560481c6b1acf8855cb2b0361d510ab692d65
SiddharthKoyugura <koyugurasiddhardha@gmail.com>
2023-04-01 14:20:19 +0530

    MC

--- app.py
+++ app.py
@@ -8,5 +8,17 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        cname = request.form.get("cname")
+        password = request.form.get("password")
+        ename = request.form.get("ename")
+        print(cname, password, ename)
+
+    return render_template("test.html")
+
+# @app.route("")
+
 if __name__ == "__main__":
     app.run(debug=True, host="localhost", port=3000)
