33491d0c9967a713f3c12d2ef83e30dc8722941b
SiddharthKoyugura <koyugurasiddhardha@gmail.com>
2023-04-01 13:19:16 +0530

    added static files

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "HEllo world"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="localhost", port=3000)
