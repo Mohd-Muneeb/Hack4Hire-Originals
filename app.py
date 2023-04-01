b8bb6acde19adf354d69c92e06f775c398fcaf58
SiddharthKoyugura <koyugurasiddhardha@gmail.com>
2023-04-01 14:36:04 +0530

    db1

--- app.py
+++ app.py
@@ -10,51 +10,46 @@ app.config["SECRET_KEY"] = "SDC12345"
 # Database Employees
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///employee.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db1 = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
-class User( db1.Model):
+class Employee( db.Model):
     __tablename__ = "company1"
-    id = db1.Column(db1.Integer, primary_key=True)
-    ename = db1.Column(db1.Integer, nullable=False, unique=True)
-    password = db1.Columm(db1.Text, nullable=False)
-    company = db1.Column(db1.Text, nullable=False)
-    is_admin = db1.Column(db1.Integer, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    ename = db.Column(db.Integer, nullable=False, unique=True)
+    password = db.Column(db.Text, nullable=False)
+    company = db.Column(db.Text, nullable=False)
+    is_admin = db.Column(db.Integer, nullable=False)
 
-db1.create_all()
-
-# Database Customer
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///customer.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db2 = SQLAlchemy(app)
-
-class company1(db2.Model):
+class company1(db.Model):
     __tablename__ = "company1"
-    id = db2.Column(db2.Integer, primary_key = True)
-    name = db2.Column(db2.Text, nullable = False)
-    mobile = db2.Column(db2.Integer, nullable = False)
-    email = db2.Column(db2.Text, nullable= False)
-    can_email= db2.Column(db2.Integer, nullable=False)
-    can_mobile= db2.Column(db2.Integer, nullable=False)
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.Text, nullable = False)
+    mobile = db.Column(db.Integer, nullable = False)
+    email = db.Column(db.Text, nullable= False)
+    can_email= db.Column(db.Integer, nullable=False)
+    can_mobile= db.Column(db.Integer, nullable=False)
 
-class company2(db2.Model):
+class company2(db.Model):
     __tablename__ = "company2"
-    id = db2.Column(db2.Integer, primary_key = True)
-    name = db2.Column(db2.Text, nullable = False)
-    mobile = db2.Column(db2.Integer, nullable = False)
-    email = db2.Column(db2.Text, nullable= False)
-    can_email= db2.Column(db2.Integer, nullable=False)
-    can_mobile= db2.Column(db2.Integer, nullable=False)
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.Text, nullable = False)
+    mobile = db.Column(db.Integer, nullable = False)
+    email = db.Column(db.Text, nullable= False)
+    can_email= db.Column(db.Integer, nullable=False)
+    can_mobile= db.Column(db.Integer, nullable=False)
 
-class company3(db2.Model):
+class company3(db.Model):
     __tablename__ = "company3"
-    id = db2.Column(db2.Integer, primary_key = True)
-    name = db2.Column(db2.Text, nullable = False)
-    mobile = db2.Column(db2.Integer, nullable = False)
-    email = db2.Column(db2.Text, nullable= False)
-    can_email= db2.Column(db2.Integer, nullable=False)
-    can_mobile= db2.Column(db2.Integer, nullable=False)
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.Text, nullable = False)
+    mobile = db.Column(db.Integer, nullable = False)
+    email = db.Column(db.Text, nullable= False)
+    can_email= db.Column(db.Integer, nullable=False)
+    can_mobile= db.Column(db.Integer, nullable=False)
 
-db2.create_all()
+with app.app_context():
+    db.create_all()
+
 
 @app.route("/")
 def home():
